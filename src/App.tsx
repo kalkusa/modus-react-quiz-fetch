0390097493121c84d78e14a8a4ff71935e68ed59
Arkadiusz Kałkus <kalkus@10g.pl>
2022-10-23 03:48:07 +0200

    Simple error handling for API errors.

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,36 @@ import { Product } from "./Types/Product";
 export default function App() {
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
 
   useEffect(() => {
     setLoading(true);
+    setError("");
     fetch(`${process.env.REACT_APP_API_URL}`)
       .then((response) => response.json())
-      .then((data) => {
-        setProducts(data as Product[]);
-        setLoading(false);
-      });
+      .then(
+        (data) => {
+          setProducts(data as Product[]);
+          setLoading(false);
+        },
+        (error) => {
+          console.error(error);
+          setProducts([]);
+          setError(error);
+          setLoading(false);
+        }
+      );
   }, []);
+
+  if (error) {
+    return (
+      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="text-2xl font-bold tracking-tight text-gray-900">
+          An error ocurred during fetching data from the server
+        </div>
+      </div>
+    );
+  }
 
   const firstFiveProducts = products.slice(0, 5);
 
